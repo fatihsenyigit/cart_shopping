5dfcbe0524160a0d0e9aa07970b18002148eb368
fatih <fsenyigit8@gmail.com>
2024-04-30 20:34:02 +0300

    category bar

--- src/components/CategoryBar.jsx
+++ src/components/CategoryBar.jsx
@@ -1,11 +1,68 @@
-
+import { Button } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
 
 const CategoryBar = () => {
-    return (
-        <div>
-            categories 
-        </div>
-    )
-}
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false)
+  const getCategories = async () => {
+    try {
+        setLoading(true)
+      const response = await axios.get(
+        "https://fakestoreapi.com/products/categories",
+      );
 
-export default CategoryBar
+      if (!response.data) {
+        throw new Error("something gone wrong");
+      }
+      setCategories(response.data);
+      setLoading(false)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  return (
+    <div className="categoryBar">
+      <FormControl>
+        <RadioGroup
+          row
+          aria-labelledby="demo-row-radio-buttons-group-label"
+          name="row-radio-buttons-group"
+          defaultValue="all"
+        >
+          {loading ? (
+            <h1>Loading...</h1>
+          ) : (
+            <>
+              <FormControlLabel
+                value="all"
+                control={<Radio />}
+                label="all"
+                defa
+              />
+              {categories?.map((item) => (
+                <FormControlLabel
+                  value={item}
+                  control={<Radio />}
+                  label={item}
+                  key={item}
+                />
+              ))}
+            </>
+          )}
+        </RadioGroup>
+      </FormControl>
+    </div>
+  );
+};
+
+export default CategoryBar;
